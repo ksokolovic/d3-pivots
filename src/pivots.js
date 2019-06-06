a04b4e985db1e6c98906e21dcef3862db5b47540
Kemal Sokolović <kemal.sokolovic@gmail.com>
2019-06-07 01:10:50 +0200

    Fix x-axis font size

--- src/pivots.js
+++ src/pivots.js
@@ -104,12 +104,13 @@ function pivotBarChart() {
                 })
                 .attr('x', function(d, i) {
                     let container = d3.select('body').append('svg');
-                    container.append('text').text(d);
+                    container.append('text').text(d).style('font-size', '12');
                     let size = container.node().getBBox();
                     container.remove();
 
                     return (i * xAxisWidth/xAxis.length) + ((xAxisWidth / xAxis.length)) / 2 + size.width / 8;
-                });
+                })
+                .style('font-size', '12');
         }
 
         // X-Axis pivot ticks

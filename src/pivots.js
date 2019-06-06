0816b9353cdec34b077e5adc1d0a9019e664b33c
Kemal Sokolović <kemal.sokolovic@gmail.com>
2019-06-06 11:30:41 +0200

    Declare data field

--- src/pivots.js
+++ src/pivots.js
@@ -1,18 +1,19 @@
-import {select} from 'd3';
-
 function pivotBarChart() {
     let width = 600,
         height = 800,
         margin = {top: 0, right: 0, bottom: 0, left: 0};
 
+    let data = null;
+
     function chart(selection) {
         selection.each(function(d, i) {
             console.log(d + i);
+            console.log(_.unique([1, 2, 1, 1, 3]));
             // Fit the chart with margins
             let chartWidth = width - margin.left - margin.right;
             let chartHeight = height - margin.top - margin.bottom;
 
-            let svg = select(this)
+            let svg = d3.select(this)
                 .append('svg')
                 .attr('width', width)
                 .attr('height', height);
@@ -53,6 +54,14 @@ function pivotBarChart() {
             return margin;
         }
         margin = value;
+        return chart;
+    }
+
+    chart.data = function(value) {
+        if (!arguments.length) {
+            return data;
+        }
+        data = value;
         return chart;
     }
 

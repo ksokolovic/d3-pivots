70e31d2b1c7398fdfadf30150a6a8825337170aa
Kemal Sokolović <kemal.sokolovic@gmail.com>
2019-06-06 15:50:45 +0200

    Implement y-axis and bars drawing
    Basic, no grouping of bars.

--- src/pivots.js
+++ src/pivots.js
@@ -1,51 +1,123 @@
 function pivotBarChart() {
-    let width = 600,
+    let canvas;
+
+    let width = 2000,
         height = 800,
-        margin = {top: 0, right: 0, bottom: 0, left: 0};
+        margin = {top: 10, right: 25, bottom: 10, left: 25},
+        bar = {groupOffset: 10, offset: 2};
+
+    let chartWidth,
+        chartHeight;
+
+    let canvasWidth = 0,
+        xAxisWidth = 0;
 
     let data = null,
         groupBy = null;
 
-    console.log(repeatArray([1, 2, 3], 3));
+    let y = undefined;
 
     function chart(selection) {
-        selection.each(function(d, i) {
-            console.log(d + i);
-            console.log(_.unique([1, 2, 1, 1, 3]));
+        selection.each(function() {
             // Fit the chart with margins
-            let chartWidth = width - margin.left - margin.right;
-            let chartHeight = height - margin.top - margin.bottom;
+            chartWidth = width - margin.left - margin.right;
+            chartHeight = height - margin.top - margin.bottom;
+
+            let xLabels = getXAxisLabels();
+            for (let i = xLabels.length - 1; i >= 0; --i) {
+                console.log(xLabels[i]);
+            }
+
+            canvasWidth = chartWidth - (xLabels[0].length - 1) * bar.groupOffset;
+            y = d3.scaleLinear().range([chartHeight, 0]);
+            y.domain([0, maxValue()]);
 
             let svg = d3.select(this)
                 .append('svg')
                 .attr('width', width)
                 .attr('height', height);
 
-            let chart = svg.append('g')
+            canvas = svg.append('g')
                 .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-            // Dummy line to test module
+            drawBars();
             drawXAxis();
-            chart.append('line')
-                .attr('x1', 0)
-                .attr('y1', 0)
-                .attr('x2', chartWidth)
-                .attr('y2', chartHeight)
-                .style('stroke-width', 1)
-                .style('stroke', 'black')
-                .style('fill', 'none');
+            drawYAxis();
         });
     }
 
     // #region Chart drawing
 
+    function drawBars() {
+        let colors = getRandomColorPalette(data.length);
+
+        canvas.selectAll('.bar')
+            .data(data)
+            .enter()
+            .append('rect')
+            .attr('class', 'bar')
+            .attr('x', function(d, i) {
+                let value = i * canvasWidth / data.length;
+
+                return value + bar.offset;
+            })
+            .attr('width', function() {
+                let barWidth = (canvasWidth / data.length) - bar.offset;
+                xAxisWidth += (barWidth + bar.offset);
+
+                return barWidth;
+            })
+            .attr('y', function(d) {
+                return y(d.value);
+            })
+            .attr('height', function(d) {
+                return chartHeight - y(d.value);
+            })
+            .attr('fill', function(d, i) {
+                return colors[i];
+            });
+    }
+
     function drawXAxis() {
 
+    }
+
+    function drawYAxis() {
+        let yAxis = d3.axisLeft().scale(y);
+
+        canvas
+            .append('g')
+            .attr('class', 'y axis')
+            .style('stroke-width', 1)
+            .call(yAxis);
     }
 
     // #endregion
 
     // #region Helper functions
+
+    function getRandomColorPalette(size) {
+        let palette = [];
+        
+        let letters = '0123456789ABCDEF';
+        for (let i = 0; i < size; ++i) {
+            let color = '#';
+            for (let j = 0; j < 6; ++j) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            palette.push(color);
+        }
+
+        return palette;
+    }
+
+    function maxValue() {
+        let maxPoint = _.max(data, function(point) {
+            return point.value;
+        });
+
+        return maxPoint.value;
+    }
 
     function repeatArray(array, times) {
         return [].concat(...Array.from({ length: times }, () => array));
@@ -53,6 +125,20 @@ function pivotBarChart() {
 
     function getXAxisLabels() {
         let unique = {};
+
+        for (const group of groupBy.x) {
+            unique[group] = _.unique(data, group).map(unique => unique[group]);
+        }
+
+        let xLabels = [];
+        xLabels.push(unique[groupBy.x[0]]);
+        for (let i = 1; i < groupBy.x.length - 1; ++i) {
+            let group = groupBy.x[i];
+            xLabels.push(repeatArray(unique[group], xLabels[i - 1].length));
+        }
+        xLabels.push(repeatArray(unique[groupBy.x[groupBy.x.length - 1]], unique[groupBy.x[groupBy.x.length - 1]].length));
+
+        return xLabels;
     }
 
     // #endregion

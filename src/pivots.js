d26b646725f774deafdccfc67cf48de36f0624f4
Kemal Sokolović <kemal.sokolovic@gmail.com>
2019-06-06 16:46:10 +0200

    Implement basic x-axis labels drawing

--- src/pivots.js
+++ src/pivots.js
@@ -4,7 +4,8 @@ function pivotBarChart() {
     let width = 2000,
         height = 800,
         margin = {top: 10, right: 25, bottom: 10, left: 25},
-        bar = {groupOffset: 10, offset: 2};
+        bar = {groupOffset: 10, offset: 2},
+        x = {offset: 25};
 
     let chartWidth,
         chartHeight;
@@ -13,20 +14,18 @@ function pivotBarChart() {
         xAxisWidth = 0;
 
     let data = null,
-        groupBy = null;
+        groupBy = null,
+        xLabels = null;
 
     let y = undefined;
 
     function chart(selection) {
         selection.each(function() {
+            xLabels = getXAxisLabels();
+
             // Fit the chart with margins
             chartWidth = width - margin.left - margin.right;
-            chartHeight = height - margin.top - margin.bottom;
-
-            let xLabels = getXAxisLabels();
-            for (let i = xLabels.length - 1; i >= 0; --i) {
-                console.log(xLabels[i]);
-            }
+            chartHeight = height - margin.top - margin.bottom + 15 - xLabels.length * x.offset;
 
             canvasWidth = chartWidth - (xLabels[0].length - 1) * bar.groupOffset;
             y = d3.scaleLinear().range([chartHeight, 0]);
@@ -79,7 +78,47 @@ function pivotBarChart() {
     }
 
     function drawXAxis() {
+        for (let i = 0; i < xLabels.length; ++i) {
+            let label = 'x' + i;
+            let className = 'text.' + label;
+            let xAxis = xLabels[i];
 
+            // Parametrize:
+            //   15 - offset in pixels for the first row of x-axis labels from the x-axis line
+            //   25 - offset between two consecutive x-axes' labels
+            canvas.selectAll(className)
+                .data(xAxis)
+                .enter()
+                .append('text')
+                .attr('class', label)
+                .attr('y', height - 15 - i * x.offset)
+                .attr('text-anchor', 'middle')
+                .text(function(d) {
+                    return d;
+                })
+                .attr('x', function(d, i) {
+                    let container = d3.select('body').append('svg');
+                    container.append('text').text(d);
+                    let size = container.node().getBBox();
+                    container.remove();
+
+                    return (i * xAxisWidth/xAxis.length) + ((xAxisWidth / xAxis.length)) / 2 + size.width / 8;
+                });
+        }
+
+        for (let i = 0; i < xLabels.length; ++i) {
+            let xAxis = xLabels[i];
+        }
+
+        // Add additional 0.5 pixels to the y-coordinate of the axis line
+        // to match the left y-axis tick
+        canvas.append('line')
+            .attr('x1', 0)
+            .attr('y1', chartHeight + 0.5)
+            .attr('x2', xAxisWidth)
+            .attr('y2', chartHeight + 0.5)
+            .style('stroke-width', 1)
+            .style('stroke', '#000000');
     }
 
     function drawYAxis() {
